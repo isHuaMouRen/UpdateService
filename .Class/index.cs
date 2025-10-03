026655ae575cd637446dbfff39fa305c29e54529
某人 华 <2825338723@qq.com>
2025-10-03 20:21:14 +0800

    [GLOBAL] 精简索引文件体积

--- .Class/index.cs
+++ .Class/index.cs
@@ -3,17 +3,11 @@ public class JsonConfig
     public class Root//根
     {
         public string[] root_url{ get; set; }//根url
-        public JsonConfig.Projects projects { get; set; }//项目集
+        public JsonConfig.Projects url_paths { get; set; }//项目集
     }
 
-    public class Projects
+    public class UrlPaths
     {
-        public JsonConfig.ProjectInfo test{ get; set; }//填写你的项目名，此以test为示例
-    }
-
-    public class ProjectInfo//项目信息，根据你的情况改变
-    {
-        public string repo { get; set; }//仓库
-        public string url_path { get; set; }//url路径，需与根url拼接
+        public string test{ get; set; }//填写你的项目名，此以test为示例
     }
 }
