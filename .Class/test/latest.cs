7d91e0ec3312438412714c3710ab7ae9943a2e59
某人 华 <2825338723@qq.com>
2025-10-06 18:58:57 +0800

    [ARCV4/TEST/CLASS] 添加json格式的信息  &  添加/更改类示例

--- .Class/test/latest.cs
+++ .Class/test/latest.cs
@@ -10,7 +10,7 @@ public class JsonConfig
     {
         public string latest_version { get; set; }//最新版本
         public string release_time { get; set; }//发布时间
-        public string release_log { get; set; }//更新日志
+        public string release_log { get; set; }//更新日志路径，需与根url拼接
         public string download_url { get; set; }//下载链接，通常为蓝奏云直链解析的zip压缩包
         public string release_url { get; set; }//github release链接
         public bool is_full_update { get; set; }//是否全量更新
